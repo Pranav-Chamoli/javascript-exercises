63e6895cf2e4bf820326a3eb01860cd216fe545d
Pranav Chamoli <94243121+Pranav-Chamoli@users.noreply.github.com>
2026-03-19 17:57:32 +0530

    JS Exercise 6 and 7 completed

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  //   Method 1 using array functions
+  // return string.split("").reverse().join("");
+  //   Method 2 using loops
+  let result = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    result += string[i];
+  }
+  return result;
 };
 
 // Do not edit below this line

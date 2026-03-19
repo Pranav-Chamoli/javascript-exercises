9a726987cff6680f27d0b1d342fddf5c96231a7a
Pranav Chamoli <94243121+Pranav-Chamoli@users.noreply.github.com>
2026-03-19 21:40:57 +0530

    JS Exercise 9 and 10 completed

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (arg1, arg2) {
+  let result = 0;
+  let min = Math.min(arg1, arg2);
+  let max = Math.max(arg1, arg2);
+  if (
+    arg1 < 0 ||
+    arg2 < 0 ||
+    !Number.isInteger(arg1) ||
+    !Number.isInteger(arg2)
+  ) {
+    return "ERROR";
+  } else {
+    for (let i = min; i <= max; i++) {
+      result += i;
+    }
+  }
 
+  return result;
 };
 
 // Do not edit below this line
